44fef9c4a79b0468e5884f1fa878fdc34d10dde6
snailuncle <1789500304@qq.com>
2018-02-05 00:07:49 +0800

    url输入一定要检查,不然报DNS错误

--- myproject/spiders/mydomain.py
+++ myproject/spiders/mydomain.py
@@ -4,8 +4,9 @@ import scrapy
 
 class MydomainSpider(scrapy.Spider):
     name = 'mydomain'
-    allowed_domains = ['mydomain.com']
-    start_urls = ['http://mydomain.com/']
+    # allowed_domains = ['mydomain.com']
+    # start_urls = ['http://mydomain.com/']
+    start_urls = ['http://www.baidu.com/']
 
     def parse(self, response):
         pass
