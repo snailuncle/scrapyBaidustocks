b29828a0c937040f96b628fa765006f5a0d41d92
snailuncle <1789500304@qq.com>
2018-02-06 22:47:56 +0800

    爬取腾讯HR网站

--- ITcast/ITcast/spiders/itcast.py
+++ ITcast/ITcast/spiders/itcast.py
@@ -1,8 +1,7 @@
 # -*- coding: utf-8 -*-
 import scrapy
 from ITcast.items import ITcastItem
-
-
+# --------------------------------------------------------------------------------------------------------------
 class ItcastSpider(scrapy.Spider):
     name = 'itcast'
     allowed_domains = ['itcast.cn']
@@ -34,3 +33,30 @@ class ItcastSpider(scrapy.Spider):
             # items.appedn(item)
 
         # yield items
+# --------------------------------------------------------------------------------------------------------------
+
+# class ItcastSpider(scrapy.Spider):
+#     name = 'itcast'
+#     allowed_domains = ['itcast.cn']
+#     start_urls = ['http://www.itcast.cn/channel/teacher.shtml']
+
+#     def parse(self, response):
+#         # print(response.body)
+#         node_list = response.xpath("//div[@class='li_txt']")
+
+#         # 用来存储所有item字段
+#         items = []
+#         for node in node_list:
+#             # 创建item字段对象,用来存储信息
+#             item = ITcastItem()
+#             # .extract() 将xpath对象转换为UNICODE字符串
+#             name = node.xpath("./h3/text()").extract()
+#             title = node.xpath("./h4/text()").extract()
+#             info = node.xpath("./p/text()").extract()
+#             item['name'] = name[0]
+#             item['title'] = title[0]
+#             item['info'] = info[0]
+#             items.append(item)
+#         return items
+# --------------------------------------------------------------------------------------------------------------
+
---
+++ tencent/Tencent/Tencent/spiders/tencent.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+import scrapy
+from Tencent.items import TencentItem
+
+
+class TencentSpider(scrapy.Spider):
+    name = 'tencent'
+    allowed_domains = ['tencent.com']
+    start_urls = ['http://tencent.com/']
+    baseURL = "https://hr.tencent.com/position.php?&start="
+    offset = 0
+    start_urls = [baseURL + str(offset)]
+
+    def parse(self, response):
+        node_list = response.xpath("//tr[@class='even'] | //tr[@class='odd']")
+
+        for node in node_list:
+            item = TencentItem()
+            item['positionName'] = node.xpath("./td[1]/a/text()").extract()[0].encode("utf-8")
+            item['positionLink'] = node.xpath("./td[1]/a/@href").extract()[0].encode("utf-8")
+            # item['positionType'] = node.xpath("./td[2]/text()").extract()[0].encode("utf-8")
+            if len(node.xpath("./td[2]/text()")):
+                item['positionType'] = node.xpath("./td[2]/text()").extract()[0].encode("utf-8")
+            else:
+                item['positionType'] = "---"
+            item['peopleNumber'] = node.xpath("./td[3]/text()").extract()[0].encode("utf-8")
+            item['workLocation'] = node.xpath("./td[4]/text()").extract()[0].encode("utf-8")
+            item['publishTime'] = node.xpath("./td[5]/text()").extract()[0].encode("utf-8")
+
+            yield item
+        if self.offset < 3090:
+            self.offset += 10
+            url = self.baseURL + str(self.offset)
+            yield scrapy.Request(url, callback = self.parse)
