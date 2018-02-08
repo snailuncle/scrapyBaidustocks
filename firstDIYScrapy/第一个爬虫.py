6189cf92c150eb003146e9c1cc3c319a9d832354
snailuncle <1789500304@qq.com>
2018-02-08 12:29:22 +0800

    提取信息失败,怎么提取标签中的标签呢

--- firstDIYScrapy/第一个爬虫.py
+++ firstDIYScrapy/第一个爬虫.py
@@ -1,13 +1,28 @@
 import requests
 from bs4 import BeautifulSoup
-URL = "https://zhuanlan.zhihu.com/p/21377121?refer=xmucpp"
+URL = "http://www.wise.xmu.edu.cn/people/staff"
 r = requests.get(URL)
 html = r.content
-soup = BeartifulSoup(html, 'html.parser')
-# div_people_list = soup.find('div', attrs={'class': 'people_list'})
-commentList = soup.find('div', attrs={'class': 'PostCommentList'})
+soup = BeautifulSoup(html, 'html.parser')
+# print(soup)
+div_people_list = soup.find('div', attrs={'class': 'module paged_list faculty_paged_list'})
+print(div_people_list)
+# commentList = soup.find('div', attrs={'class': 'PostCommentList'})
+# print(commentList)
 # a_s = div_people_list.find_all('a', attrs={'target': '_blank'})
-CommentItems = commentList.find_all('div', attrs={'class': 'CommentItem'})
-for info in CommentItem:
-    name = info.find()
-    theComment = 
+# CommentItems = commentList.find_all('div', attrs={'class': 'CommentItem'})
+# print(CommentItems)
+# for info in CommentItem:
+#     name = info.find()
+#     theComment = 
+# get_text(strip=True)
+# method 1
+# tags = soup.find('div').find_all('p')
+# for i in tags:
+#     # do something here
+#     print i.text
+
+# # method 2
+# print soup.find('div').find('p').text
+# print soup.find('div').find('p').find_next('p').text
+# a.table.tr.td.string
