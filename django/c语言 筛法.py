01e2c25ed85a5646418bbeb3f64364024aa9a048
snailuncle <1789500304@qq.com>
2018-02-22 18:54:22 +0800

    质数定义法和筛选法的---时间对比

--- django/c语言 筛法.py
+++ django/c语言 筛法.py
@@ -1,21 +1,17 @@
-#include <stdio.h>  
-int a[2000005]={0};//初始化  
-int main()  
-{  
-    int n,i,j;  
-    /*筛法求素数*/  
-    for(i=2;i<=1000003;i++)  
-        if(a[i]==0)//如果是标记过的，则直接执行i++   
-            for(j=i+i;j<=2000005;j=j+i)  
-                a[j]=1;//依次筛去i的倍数，即标记合数   
-    while(scanf("%d",&n)!=EOF)  
-    {  
-        if(n==0)  
-            break;  
-        for(i=2;i<=n;i++)  
-            if(a[i]==0)   
-                printf("%d ",i);//输出未标记的下标（素数）  
-        printf("\n");  
-    }  
-    return 0;  
-}  
+def eladuosai(n):
+    l = list(range(1,n+1))     # l是一个 1-n  的 list
+    print(l)
+    l[0] = 0
+    print(l)
+    for i in range(2,n+1):      # 从2开始的所有数
+        print("现在判断的数字是i=", str(i-1), "   l["+str(i-1)+"]=", l[i-1])
+        if l[i-1] != 0 :        # 0
+           
+            for j in range(i*2,n+1,i):  # start = i*2, end = n+1, step = i     i=2时, start = 4, end = 11, i = 2
+                print(j)
+                l[j-1] = 0
+    result = [x for x in l if x != 0]  # 遍历一次l  如果不等于0 就返回这个数  这就是所有的素数
+    return result
+
+e = eladuosai(100)
+print(e)
